a07c1cfbcd589995cac77058483db9e3b9c5588d
caowens <caowens555@yahoo.com>
2024-07-19 11:39:59 -0400

    Add JS basics

---
+++ Javascript/script.js
@@ -0,0 +1,61 @@
+
+// let number = 3;
+// console.log(number);
+// number = 5;
+// console.log(number);
+// let isCat = true;
+// console.log(isTrue);
+
+// Number 565
+// boolean true or false
+// string: " i am a string"
+
+// const word = "Alex";
+// console.log(word);
+// word = "Derek";
+// console.log(word);
+// const password = "12345";
+
+document.getElementById("box").innerHTML = "<h1>Derek is awesome</h1>";
+
+document.getElementById("box").addEventListener("click", function() {
+    alert("I got clicked");
+});
+
+document.getElementById("box").style = "cursor: pointer";
+
+// let number = 5;
+
+// if (number === 10) {
+//     console.log("Yay!");
+// } else if (number === 5) {
+//     console.log("Five!");
+// } else {
+//     console.log("Boo!");
+// }
+
+// console.log("2" == 2);
+
+// let arr = [1, 2, "three", "four", false];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach((item) => {
+//     console.log(item);
+// })
+
+// function
+// function printItem(item) {
+//     console.log(item);
+// }
+
+// arrow function
+// const printItem = (item) => {
+//     console.log(item);
+// }
+
+
+
+// printItem("Alex");
